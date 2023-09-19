fb0341671c8b3dbfe60a1530acc2e710358a70be
Shin Changyu <scv1702@naver.com>
2023-09-19 20:19:00 +0900

    Solve 110. Balanced Binary Tree

---
+++ src/main/java/scv1702/easy/BalancedBinaryTree/BalancedBinaryTree.java
@@ -0,0 +1,21 @@
+package scv1702.easy.BalancedBinaryTree;
+
+import scv1702.data.TreeNode;
+
+// time complexity: O(N). Because checkHeight() has to check whether the subtree is balacned or not, the nodes in the tree visited at once.
+// space complexity: O(H). Because checkHeight() is called recursively from the root to the terminal to compute the height of the tree 
+public class BalancedBinaryTree {
+    public boolean isBalanced(TreeNode root) {
+        return checkHeight(root) != Integer.MIN_VALUE;
+    }
+
+    public int checkHeight(TreeNode node) {
+        if (node == null) return -1;
+        int leftHeight = checkHeight(node.left);
+        if (leftHeight == Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        int rightHeight = checkHeight(node.right);
+        if (rightHeight == Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        if (Math.abs(leftHeight - rightHeight) > 1) return Integer.MIN_VALUE;
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
