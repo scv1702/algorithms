6b63b63f81d9c34773f28e11293ab55c8bc1fb93
Shin Changyu <scv1702@naver.com>
2023-09-21 14:56:19 +0900

    Solve 112. Path Sum

---
+++ src/main/java/scv1702/easy/PathSum/PathSum.java
@@ -0,0 +1,15 @@
+package scv1702.easy.PathSum;
+
+import scv1702.data.TreeNode;
+
+// time complexity: O(N)
+// space complexity: O(H)
+// idea: subtract the value of the node from targetSum
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null) return targetSum == root.val;
+        return hasPathSum(root.left, targetSum - root.val) || 
+        hasPathSum(root.right, targetSum - root.val);
+    }
+}
