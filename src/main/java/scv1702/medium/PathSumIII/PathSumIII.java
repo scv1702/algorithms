9632c7c8280f365d6d010fb87d84fb41569dca8d
Shin Changyu <scv1702@naver.com>
2023-09-21 18:51:14 +0900

    Solve 437. Path Sum III

---
+++ src/main/java/scv1702/medium/PathSumIII/PathSumIII.java
@@ -0,0 +1,26 @@
+package scv1702.medium.PathSumIII;
+
+import java.util.*;
+import scv1702.data.TreeNode;
+
+public class PathSumIII {
+        int path = 0;
+    Map<Long, Integer> lookup = new HashMap<>();
+
+    public int pathSum(TreeNode root, int targetSum) {
+        if (root == null) return 0;
+        lookup.put(0L, 1);
+        dfs(root, 0L, targetSum);
+        return path;
+    }
+
+    public void dfs(TreeNode root, long prefixSum, int targetSum) {
+        if (root == null) return ;
+        prefixSum += root.val;
+        path += lookup.getOrDefault(prefixSum - targetSum, 0);
+        lookup.put(prefixSum, lookup.getOrDefault(prefixSum, 0) + 1);
+        dfs(root.left, prefixSum, targetSum);
+        dfs(root.right, prefixSum, targetSum);
+        lookup.put(prefixSum, lookup.get(prefixSum) - 1);
+    }
+}
