8db60f153b4341f65f9e41127e26d48522919760
Shin Changyu <scv1702@naver.com>
2023-10-05 16:15:19 +0900

    Solve 100. Same Tree

---
+++ src/main/java/scv1702/easy/SameTree.java
@@ -0,0 +1,40 @@
+package scv1702.easy;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import scv1702.data.TreeNode;
+
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null)
+            return p == q;
+        Deque<TreeNode> pq = new ArrayDeque<>();
+        Deque<TreeNode> qq = new ArrayDeque<>();
+        pq.offerLast(p);
+        qq.offerLast(q);
+        while (!pq.isEmpty() && !qq.isEmpty()) {
+            TreeNode pn = pq.pollLast();
+            TreeNode qn = qq.pollLast();
+            if (pn.val != qn.val)
+                return false;
+            if (pn.left == null || qn.left == null) {
+                if (pn.left != qn.left)
+                    return false;
+            }
+            if (pn.right == null || qn.right == null) {
+                if (pn.right != qn.right)
+                    return false;
+            }
+            if (pn.left != null)
+                pq.offerLast(pn.left);
+            if (pn.right != null)
+                pq.offerLast(pn.right);
+            if (qn.left != null)
+                qq.offerLast(qn.left);
+            if (qn.right != null)
+                qq.offerLast(qn.right);
+        }
+        return pq.isEmpty() && qq.isEmpty();
+    }
+}
