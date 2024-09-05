cc9eccefe55ca7447c5369f59af8c2d689657e08
Changyu Shin <scv1702@naver.com>
2024-09-05 15:03:53 +0900

    solve: 새싹(25083번) 해결

---
+++ AcmicpcNet/Bronze/N25083/Main.java
@@ -0,0 +1,17 @@
+package AcmicpcNet.Bronze.N25083;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(
+            """
+                     ,r'"7
+            r`-_   ,'  ,/
+             \\. ". L_r'
+               `~\\/
+                  |
+                  |
+            """
+        );
+    }
+}
