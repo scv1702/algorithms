a642b500ef472eaa5bdd15eaa7b2d5900256fd24
scv1702 <scv1702@naver.com>
2024-11-15 16:32:35 +0900

    solve: 올바른 괄호 해결

--- Programmers/Level2/올바른 괄호.java
+++ Programmers/Level2/올바른 괄호.java
@@ -15,4 +15,23 @@ class Solution {
         }
         return stack.isEmpty();
     }
+
+    boolean solutionUsingStack(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(c);
+            }
+            if (c == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
 }
