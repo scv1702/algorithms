6b3039eb9a5e07d70e166edd74d1fe4706ae31b8
scv1702 <scv1702@naver.com>
2024-11-15 17:19:10 +0900

    solve: JadenCase 문자열 만들기 해결

--- Programmers/Level2/JadenCase 문자열 만들기.java
+++ Programmers/Level2/JadenCase 문자열 만들기.java
@@ -20,4 +20,24 @@ class Solution {
         }
         return sb.toString();
     }
+
+    public String solutionV2(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                sb.append(s.charAt(i));
+                continue;
+            }
+            if (i == 0 || s.charAt(i-1) == ' ') {
+                if (Character.isAlphabetic(s.charAt(i))) {
+                    sb.append(Character.toUpperCase(s.charAt(i)));
+                } else {
+                    sb.append(s.charAt(i));
+                }
+            } else {
+                sb.append(Character.toLowerCase(s.charAt(i)));
+            }
+        }
+        return sb.toString();
+    }
 }
