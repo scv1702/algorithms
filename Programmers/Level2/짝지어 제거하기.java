42f3592d169b55d637be1f99cff86f652ad44db3
scv1702 <scv1702@naver.com>
2024-11-15 18:02:41 +0900

    solve: 짝지어 제거하기 해결

--- Programmers/Level2/짝지어 제거하기.java
+++ Programmers/Level2/짝지어 제거하기.java
@@ -13,4 +13,25 @@ class Solution {
         }
         return stack.size() == 0 ? 1 : 0;
     }
+
+    public int solution(String s) {
+        // 짝지어 제거했을 때 모두 제거할 수 있을까?
+        // () 판별 문제랑 비슷한듯?
+        // 문자 c로 시작했으면 문자 c로 닺혀야 함
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (stack.isEmpty()) {
+                stack.push(c);
+                continue;
+            }
+            if (stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty() ? 1 : 0;
+    }
 }
