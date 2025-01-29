019da0e9574b7550cf0cbd24179025eef7a07df5
scv1702 <scv1702@naver.com>
2025-01-29 23:26:16 +0900

    solve: 학점계산(2754번) 해결

---
+++ AcmicpcNet/Bronze/N2754/Main.java
@@ -0,0 +1,37 @@
+package AcmicpcNet.Bronze.N2754;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Map<String, Double> gradeMap = new HashMap<>();
+        gradeMap.put("A+", 4.3);
+        gradeMap.put("A0", 4.0);
+        gradeMap.put("A-", 3.7);
+        gradeMap.put("B+", 3.3);
+        gradeMap.put("B0", 3.0);
+        gradeMap.put("B-", 2.7);
+        gradeMap.put("C+", 2.3);
+        gradeMap.put("C0", 2.0);
+        gradeMap.put("C-", 1.7);
+        gradeMap.put("D+", 1.3);
+        gradeMap.put("D0", 1.0);
+        gradeMap.put("D-", 0.7);
+        gradeMap.put("F", 0.0);
+
+        String grade = br.readLine();
+
+        bw.write(String.format("%.1f", gradeMap.get(grade)));
+        bw.flush();
+        bw.close();
+    }
+}
