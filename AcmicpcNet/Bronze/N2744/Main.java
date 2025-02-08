eb2834b6f4232efa4e5dddd64c10c1ef240a98fa
scv1702 <scv1702@naver.com>
2025-02-08 09:02:26 +0900

    solve: 대소문자 바꾸기(2744번) 해결

---
+++ AcmicpcNet/Bronze/N2744/Main.java
@@ -0,0 +1,29 @@
+package AcmicpcNet.Bronze.N2744;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isUpperCase(chars[i])) {
+                chars[i] = Character.toLowerCase(chars[i]);
+            } else {
+                chars[i] = Character.toUpperCase(chars[i]);
+            }
+        }
+        String ans = new String(chars);
+
+        bw.write(ans);
+        bw.flush();
+        bw.close();
+    }
+}
